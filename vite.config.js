8a778039cc8b6c07592de89bec244105adab740e
mrSharon <oleratomatlou1@gmail.com>
2026-01-31 01:46:25 +0200

    first iteration needs me to fix other features

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: mandipa.a.hlabano,
   resolve: {
     alias: {
       "@": path.resolve('./src'),
